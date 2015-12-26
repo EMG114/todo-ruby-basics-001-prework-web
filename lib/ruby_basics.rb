e119450dcc3d7abb7e7f6c5db76c104719393466
erica g <keka@ericas-MacBook-Air.local>
2015-12-25 19:15:48 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,42 @@
 def division(num1, num2)
+    num1=42
+    num2=7
+    return num1/(num2)
+    
  
 end
 
-def assign_variable(value)
+def assign_variable(nam)
+    puts c= nam
+    return c
   
 end
 
-def argue
+def argue (x="Always right!")
+    return "#{x}"
   
 end
 
-def greeting
+def greeting (greets, name)
+    puts "#{greets}" + " " + "#{name}"
 
 end
 
-def return_a_value
+def return_a_value (a="Nice")
+    return "#{a}"
+    
 
 end
 
-def last_evaluated_value
+def last_evaluated_value (phrase="expert")
+    puts phrase
+    return "#{phrase}"
 
 end
 
-def pizza_party
+def pizza_party (topping="cheese")
+    puts "#{topping}"
+    return topping
+
 
 end
